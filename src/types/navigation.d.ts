0c1d1c35364063b9f9e7cf80a4a348ca4e4db0d9
Bruno Martins <bruno.martins.cesfi@gmail.com>
2025-06-19 13:03:56 -0300

    Define route types

---
+++ src/types/navigation.d.ts
@@ -0,0 +1,12 @@
+export type RootStackParamList = {
+    home: undefined
+    product: undefined
+}
+
+declare global {
+    namespace ReactNavigation {
+        interface RootParamList extends RootStackParamList {
+
+        }
+    }
+}
